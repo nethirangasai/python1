a76a14816babf1176fc75bfa6b207a701097881c
ranga <rangasai.nethi@gmail.com>
2020-11-13 20:05:12 +0530

    test4 new class added and updated functionalities in other files

--- test2.py
+++ test2.py
@@ -1,2 +1,3 @@
 class test2:
   print("sai-test2")
+  print("test2 new print added")
--- test3.py
+++ test3.py
@@ -1,4 +1,4 @@
 class test3:
     print("sai-test3")
     print("test3 new print added")
-    print("test3 new secongd print added")
+    print("test3 new second print added")
